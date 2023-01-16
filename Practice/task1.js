0c2e358b7ab0fbe7a62c93dba6ad608b0d5e9245
Barotov Ogabek <76640273+ogabek0201@users.noreply.github.com>
2023-01-16 14:51:58 +0500

    delete folder

--- Practice/task1.js
+++
@@ -1,4 +0,0 @@
-let a=prompt()
-let b=prompt()
-let c= (a.length==b.length)?true:false;
-console.log(c);
--- Practice/task2.js
+++
@@ -1,3 +0,0 @@
-let txt="minencraft"
-    let result=txt.substring(4,1)
-    console.log(result)
